ccaf8a7c0d7647b8ed4d99ed7166ef6b2c4a54f6
Erik Svensson <eosvensson@gmail.com>
2022-08-17 08:54:31 +0200

    Added MovieEvent

---
+++ src/main/java/com/example/netflux/domain/MovieEvent.java
@@ -0,0 +1,13 @@
+package com.example.netflux.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class MovieEvent {
+    private String movieId;
+    private LocalDateTime dateTime;
+}
