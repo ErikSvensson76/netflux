7cbc125a6de8af01290d2eda1ee69a0463b87db3
Erik Svensson <eosvensson@gmail.com>
2022-08-17 09:01:40 +0200

    Added MovieService.streamMovieEvents method

--- src/main/java/com/example/netflux/service/MovieService.java
+++ src/main/java/com/example/netflux/service/MovieService.java
@@ -1,6 +1,7 @@
 package com.example.netflux.service;
 
 import com.example.netflux.domain.Movie;
+import com.example.netflux.domain.MovieEvent;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -8,4 +9,6 @@ public interface MovieService {
     Mono<Movie> getById(String id);
 
     Flux<Movie> findAll();
+
+    Flux<MovieEvent> streamMovieEvents(String id);
 }
--- src/main/java/com/example/netflux/service/MovieServiceImpl.java
+++ src/main/java/com/example/netflux/service/MovieServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.netflux.service;
 
 import com.example.netflux.domain.Movie;
+import com.example.netflux.domain.MovieEvent;
 import com.example.netflux.repositories.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -14,12 +18,18 @@ public class MovieServiceImpl implements MovieService {
     private final MovieRepository movieRepository;
 
     @Override
-    public Mono<Movie> getById(String id) {
+    public Mono<Movie> getById(final String id) {
         return movieRepository.findById(id);
     }
 
     @Override
     public Flux<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    @Override
+    public Flux<MovieEvent> streamMovieEvents(final String id) {
+        return Flux.<MovieEvent>generate(generator -> generator.next(new MovieEvent(id, LocalDateTime.now())))
+                .delayElements(Duration.ofSeconds(1));
     }
 }
